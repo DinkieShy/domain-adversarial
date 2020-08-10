97aab69e98fcdb89c31800d7ba3a81adb2cd3607
Dinkie <david.churchill72@gmail.com>
2020-08-10 13:11:24 +0100

    commit changes to whileTrain.py and update README with install instructions

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ This repository includes source code for adding a domain adversarial head to a p
 To run: add .csv files for training and validation sets in `input/`, then create `input/data/` and fill with images
 
 This was created for the Global Wheat Detection 2020 challenge, https://www.kaggle.com/c/global-wheat-detection/overview
+
+# Installation
+Prerequisites:
+-Cuda 10.0, https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html
+-Latest version of pip, `pip install --upgrade pip`
+
+Install:
+Clone this repository, then run `pip3 install -f requirements.txt` in the project directory
+
+OPTIONAL: You may wish to create a new virtual environment to install these packages, by running `python3 -m venv envName`.
+To use this virtual environment, you must run `source envName/bin/activate` in any new terminal (not necessary immediately after creating the environment). 
+(You can replace `envName` in both commands with whatever you want your environment to be called)
--- whileTrain.py
+++ whileTrain.py
@@ -19,11 +19,9 @@ from torchvision.models.detection.rpn import AnchorGenerator
 from torch.utils.data import DataLoader, Dataset
 from torch.utils.data.sampler import SequentialSampler
 
-import torch.nn as nn
-import torch.nn.functional as F
-
 from util.Evaluation import calculatePrecision
 from datasets.GWD import WheatDataset
+from models.domainAdversarial import DomainAdversarialHead
 
 INPUT_DIR = "./input/"
 OUTPUT_DIR = "./output/"
@@ -107,52 +105,6 @@ def get_valid_transform():
     ], bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']})
 
 #--- Creating the model -----------------------------------------------------------------------------------------------------
-
-class GradientReversalLayer(nn.Module):
-    def __init__(self):
-        super(GradientReversalLayer, self).__init__()
-    
-    def forward(self, x):
-        return x
-
-    def backward(self, x):
-        return -x
-
-class Classifier(nn.Module):
-  def __init__(self):
-    super().__init__()
-    self.feature_extractor = nn.Sequential(
-        nn.Conv2d(3, 10, kernel_size=5),
-        nn.MaxPool2d(2),
-        nn.ReLU(),
-        nn.Conv2d(10, 20, kernel_size=5),
-        nn.MaxPool2d(2),
-        nn.Dropout2d(),
-    )
-    
-    self.classifier = nn.Sequential(
-        nn.Linear(320, 50),
-        nn.ReLU(),
-        nn.Dropout(),
-        nn.Linear(50, 10),
-    )
-
-  def forward(self, x):
-    features = self.feature_extractor(x)
-    features = features.view(x.shape[0], -1)
-    logits = self.classifier(features)
-    return logits
-
-class DomainAdversarialHead(nn.Module):
-  def __init__(self):
-    super().__init__()
-    self.gradientReversal = GradientReversalLayer()
-    self.classifier = Classifier()
-
-  def forward(self, x):
-    y = self.gradientReversal.forward(x)
-    y = self.classifier(y)
-    return y
 
 model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 
