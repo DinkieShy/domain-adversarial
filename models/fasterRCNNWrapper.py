2307b6ba82eef5291bfdbd6057c728d481c9fd49
Dinkie <david.churchill72@gmail.com>
2020-08-13 14:31:17 +0100

    fasterRCNNWrapper class in models/

---
+++ models/fasterRCNNWrapper.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
+
+class customFasterRCNN(nn.Module):
+    """
+    Standard classification + bounding box regression layers
+    for Fast R-CNN.
+    Arguments:
+        in_channels (int): number of input channels
+        num_classes (int): number of output classes (including background)
+    """
+
+    def __init__(self, in_channels, num_classes):
+        super(customFasterRCNN, self).__init__()
+        self.cls_score = nn.Linear(in_channels, num_classes)
+        self.bbox_pred = nn.Linear(in_channels, num_classes * 4)
+        self.hasOutput = False
+
+    def forward(self, x):
+        if x.dim() == 4:
+            assert list(x.shape[2:]) == [1, 1]
+        x = x.flatten(start_dim=1)
+        scores = self.cls_score(x)
+        bbox_deltas = self.bbox_pred(x)
+
+        if not self.hasOutput:
+            print("customerFasterRCNN.faster")
+            self.hasOutput = True
+
+        return scores, bbox_deltas
