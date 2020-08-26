12625621122e63a63f32fe66be3a76e764622448
Dinkie <david.churchill72@gmail.com>
2020-08-26 16:26:49 +0100

    add util script for separating 3 domains for testing/training

---
+++ util/SplitDataByDomain.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import csv
+import numpy as np
+import math
+from shutil import copyfile
+import json
+
+train_df = pd.read_csv("./input/fullTrain.csv")
+
+domainsToRemove = ["usask_1", "inrae_1", "rres_1"]
+
+# # Print details of domains for choosing domains to remove
+
+# domains = []
+# domainCounts = []
+# for i in range(len(train_df)):
+#     source = train_df['source'][i]
+#     if source in domains:
+#         domainCounts[domains.index(source)] += 1
+#     else:
+#         domains.append(source)
+#         domainCounts.append(1)
+
+# print(domains)
+# print(domainCounts)
+# print(sum(domainCounts))
+
+train = pd.DataFrame(columns=train_df.columns)
+test  = pd.DataFrame(columns=train_df.columns)
+
+print("Splitting data...")
+
+mask = train_df['source'].map(lambda source: source in domainsToRemove)
+
+train = train_df[~mask]
+test = train_df[mask]
+
+print("Saving...")
+
+train.to_csv("./input/trainDomain.csv")
+test.to_csv("./input/testDomain.csv")
+
+print("Done.")
